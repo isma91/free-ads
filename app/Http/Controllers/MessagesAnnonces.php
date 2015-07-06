b997c3c7dab375b07a8251bbfb0864414aa0d795
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 12:45:33 +0200

    message controller

---
+++ app/Http/Controllers/MessagesAnnonces.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* MessagesController.php
+* 
+* PHP Version 5.2
+*
+* @category Controleur
+* @package  Controleur
+* @author   aydogm_i <ismail.aydogmus@epitech.eu>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     http://localhost:8080/rendu/Projets_MVC_Cloud/app/Http/Controllers/MessagesController.php
+*/
+namespace freeads\Http\Controllers;
+
+use freeads\Http\Requests;
+use freeads\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+/**
+* Class Message
+*
+* Class permettant d'ajouter les membres
+* 
+* PHP Version 5.2
+*
+* @category Controleur
+* @package  Controleur
+* @author   aydogm_i <ismail.aydogmus@epitech.eu>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     http://localhost:8080/rendu/Projets_MVC_Cloud/app/Http/Controllers/MessagesController.php
+*/
+class MessagesAnnonces extends Controller
+{
+}
