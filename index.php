e816d54cf7eb1e212655dfc2819e3c289ae59134
aydogm_i <ismaydogmus@gmail.com>
2015-07-06 12:18:12 +0200

    the redirection

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Index.php
+* 
+* PHP Version 5.2
+*
+* @category View
+* @package  View
+* @author   aydogm_i <ismail.aydogmus@epitech.eu>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+* @link     http://localhost:8080/rendu/Projets_MVC_Cloud/index.php
+*/
+header("Location: public/index.php/accueil");
+?>
